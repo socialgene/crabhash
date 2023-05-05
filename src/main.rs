8450a50d97cddeed6fc3a2cac197f46bff620d5b
chasemc <18691127+chasemc@users.noreply.github.com>
2023-05-05 09:37:01 -0500

    WIP-removes functionality but fixes hashing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 use std::env;
 use std::fs::File;
 use seq_io::fasta::{Record, RefRecord};
-use std::io::Write;
+use std::io::{BufWriter, Write};
+use std::fs;
+
 use std::path::PathBuf;
+use std::path::Path;
 use glob::glob_with;
-use glob::MatchOptions;
 use sha2::{Sha512, Digest};
 use base64;
 //use grep_cli::stdout; //https://users.rust-lang.org/t/why-is-this-rust-loop-3x-slower-when-writing-to-disk/30489/3
@@ -14,84 +16,63 @@ use scoped_threadpool::Pool;
 
 mod filehandling;
 
-// Import the base64 crate Engine trait anonymously so we can
-// call its methods without adding to the namespace.
-use base64::engine::Engine as _;
-use base64::engine::general_purpose::STANDARD as BASE64;
+use base64::{Engine as _, engine::general_purpose};
 
 
 
 
-fn parse_config(args: &[String]) -> &str {
-    let dirpath = &args[1];
-    dirpath
-}
-
-fn hash_and_write(record: &RefRecord, mut fasta_file: &File, mut tsv_file: &File) {
-    let mut hasher = Sha512::new();
-    let seq_str = String::from_utf8_lossy(record.seq()).to_string().replace('\n', "").replace('\r', ""); 
-    hasher.update(&seq_str);
-    let result = hasher.finalize();
-    let ss = &result[0..24];
-    let encoded = BASE64.encode(&ss);
-    let mut owned_string: String = "".to_owned();
-    
-    owned_string.push_str(">");
-    owned_string.push_str(&encoded);
-    owned_string.push_str("\n");
-    owned_string.push_str(&seq_str);
-    owned_string.push_str("\n");
-    fasta_file.write_all(&owned_string.as_bytes()).expect("write failed");
-    
-    let mut owned_string2: String = "".to_owned();
-    owned_string2.push_str(&encoded);
-    owned_string2.push_str("\t");
-    owned_string2.push_str(record.id().unwrap());
-    owned_string2.push_str("\n");
-    tsv_file.write_all(&owned_string2.as_bytes()).expect("write failed");
-}
-
-fn create_outpaths(args: &[String], filename_string: &String) -> (File, File)  {
-    let dirpath = &args[2];
-    let fasta_pathbuf: PathBuf = [dirpath, &[&filename_string, ".fasta"].join("")].iter().collect();
-    let tsv_pathbuf: PathBuf = [dirpath, &[&filename_string, ".tsv"].join("")].iter().collect();     
-    let fasta_file = std::fs::File::create(&fasta_pathbuf).expect("create failed");
-    let tsv_file = std::fs::File::create(&tsv_pathbuf).expect("create failed");
-
-    return (fasta_file, tsv_file) ;
-}
 
 fn main()  {
     let args: Vec<String> = env::args().collect();
-    let dirpath = parse_config(&args);
 
-    let options = MatchOptions {
-        case_sensitive: false,
-        require_literal_separator: false,
-        require_literal_leading_dot: false,
-    };
+    let input_file = Path::new(&args[1]);
     
-    let max_workers = args[3].parse::<u32>().unwrap();
-    let mut pool = Pool::new( max_workers );
-    //let files: Vec<String> = Vec::from( &args[1..] );
-        // Match the glob pattern, filtering out bad paths      
-    pool.scoped( |scoped| {          
-        for entry in glob_with(&dirpath, options).unwrap().filter_map(Result::ok) {
-            // get outpath
-            let filename = entry.file_name();
-            let filename_string: String = filename.unwrap().to_str().unwrap().into();               
-            let (fasta_file,tsv_file) = create_outpaths(&args, &filename_string);                       
-            scoped.execute( move || {
-                        // create output files
-                        let (mut input, _) = filehandling::get_input( &filename_string).unwrap();
-                        //let buf = get_reader(&entry);
-                        let mut reader = seq_io::fasta::Reader::new(&mut input);
-                        while let Some(record) = reader.next() {
-                            let record = record.unwrap();
-                            hash_and_write(&record, &fasta_file, &tsv_file)                      
-                        }
-                    }
-                );
-            }
-        }); 
+    let filename = &input_file.file_stem().unwrap();
+    let filename_string = &input_file.file_stem().unwrap().to_str().unwrap();
+
+    let outdir = Path::new(&args[2]);
+
+    let mut tsv_pathbuf = PathBuf::from(&outdir);
+
+    tsv_pathbuf.push(&[&filename_string, ".tsv"].join(""));
+    
+    let (mut input, _) = filehandling::get_input(&args[1]).unwrap();
+
+    let mut reader = seq_io::fasta::Reader::new(&mut input);
+
+    let mut n = 0;
+    print!("Finished 0 million sequences");
+    let tt = std::fs::File::create(&tsv_pathbuf).expect("create failed");
+    let mut tsv_file = BufWriter::new(tt);
+
+
+    while let Some(record) = reader.next() {
+        let record2 = record.unwrap();
+        let mut hasher = Sha512::new();
+        let seq_str = String::from_utf8_lossy(record2.seq()).to_string().replace('\n', "").replace('\r', ""); 
+        hasher.update(&seq_str);
+        let result = hasher.finalize();
+        let ss = &result[0..24];
+        // Conistent with Python's RFC 3548 base64.urlsafe_b64encode
+        let encoded = general_purpose::URL_SAFE_NO_PAD.encode(&ss);
+
+        let mut s = String::new();
+
+        s.push_str(&encoded);
+        s.push_str("\t");
+        s.push_str(record2.id().unwrap());
+        s.push_str("\n");
+        
+        if n % 1000000 == 0 {
+            print!("\r Finished {} x million sequences", n/1000000);
+        }
+        n += 1;
+        tsv_file.write_all(&s.as_bytes()).expect("write failed");
+
+
+    }   
+
+
+    
+    tsv_file.flush().unwrap();
 }
